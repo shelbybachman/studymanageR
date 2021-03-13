130cebe5ce0a7b4af3309b6ecca7948204890dd8
Shelby Bachman <11279603+shelbybachman@users.noreply.github.com>
2021-03-13 12:47:28 -0800

    update initialize_project function

--- functions/initialize_project.R
+++ functions/initialize_project.R
@@ -8,44 +8,42 @@
 
 # tba: initialize git repository via command line
 
-setup_project <- function(study_name, home_dir, 
-                          verbose = TRUE
-                          ) {
+initialize_project <- function(study_name, home_dir, verbose = TRUE) {
   
   ###### create project root directory 
-  if (dir.exists(here::here(home_dir, study_name))){
+  if (dir.exists(file.path(home_dir, study_name))){
     stop('Project root directory already exists. Try again!')
   } else {
-    dir.create(here::here(home_dir, study_name), showWarnings = TRUE)
+    dir.create(file.path(home_dir, study_name), showWarnings = TRUE)
     if (verbose == TRUE) {
-      message(paste('created project root directory: ', here::here(home_dir, study_name), ' ...', sep = ''))
+      message(paste('created project root directory: ', file.path(home_dir, study_name), ' ...', sep = ''))
     }
   }
   
   ###### create project subdirectory -- data
   # based on this standard: Wilson et al., https://doi.org/10.1371/journal.pcbi.1005510
-  dir.create(here::here(home_dir, study_name, 'data'))
+  dir.create(file.path(home_dir, study_name, 'data'))
   if (verbose == TRUE) {
     message(paste('created data subdirectory: ', study_name, '/data', ' ...', sep = ''))
   }
-  dir.create(here::here(home_dir, study_name, 'data', 'derivatives'))
-  dir.create(here::here(home_dir, study_name, 'data', 'rawdata'))
-  dir.create(here::here(home_dir, study_name, 'data', 'sourcedata'))
+  dir.create(file.path(home_dir, study_name, 'data', 'derivatives'))
+  dir.create(file.path(home_dir, study_name, 'data', 'rawdata'))
+  dir.create(file.path(home_dir, study_name, 'data', 'sourcedata'))
   
   ###### create project subdirectory - doc
-  dir.create(here::here(home_dir, study_name, 'doc'))
+  dir.create(file.path(home_dir, study_name, 'doc'))
   if (verbose == TRUE) {
     message(paste('created doc subdirectory: ', study_name, '/doc', ' ...', sep = ''))
   }
   
   ###### create project subdirectory - results
-  dir.create(here::here(home_dir, study_name, 'results'))
+  dir.create(file.path(home_dir, study_name, 'results'))
   if (verbose == TRUE) {
     message(paste('created results subdirectory: ', study_name, '/results', ' ...', sep = ''))
   }
   
   ###### create project subdirectory - src
-  dir.create(here::here(home_dir, study_name, 'src'))
+  dir.create(file.path(home_dir, study_name, 'src'))
   if (verbose == TRUE) {
     message(paste('created src subdirectory: ', study_name, '/src', ' ...', sep = ''))
   }
@@ -53,10 +51,10 @@ setup_project <- function(study_name, home_dir,
   ###### build studymanageR-specific json file
   project_info <- list(
     study_name = study_name,
-    root_dir = here::here(home_dir, study_name)
+    root_dir = file.path(home_dir, study_name)
   )
   project_info <- rjson::toJSON(project_info)
-  write(project_info, here::here(home_dir, study_name, paste(study_name, '_info.json', sep = '')))
+  write(project_info, file.path(home_dir, study_name, paste(study_name, '_info.json', sep = '')))
   if (verbose == TRUE) {
     message(paste('saved project metadata: ', study_name, '/', study_name, '_info.json', ' ...', sep = ''))
   }
