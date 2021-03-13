cbc22f6e3f3e85993f19f7f02685c6e6f89a21a1
Shelby Bachman <11279603+shelbybachman@users.noreply.github.com>
2021-03-13 12:35:50 -0800

    move data subdirectory creation to create_data_dirs

--- functions/initialize_project.R
+++ functions/initialize_project.R
@@ -9,12 +9,6 @@
 # tba: initialize git repository via command line
 
 setup_project <- function(study_name, home_dir, 
-                          n_sessions = 1,
-                          session_labels = NA,
-                          n_groups = 1,
-                          group_labels = NA,
-                          n_pergroup = 1,
-                          first_subject_pergroup = 1001,
                           verbose = TRUE
                           ) {
   
@@ -38,33 +32,6 @@ setup_project <- function(study_name, home_dir,
   dir.create(here::here(home_dir, study_name, 'data', 'rawdata'))
   dir.create(here::here(home_dir, study_name, 'data', 'sourcedata'))
   
-  ### create subject & session subdirectories
-  # according to bids specification: https://bids.neuroimaging.io/specification
-  sub_dirs <- NULL
-  for (ii in 1:n_groups) {
-    sub_dirs <- append(sub_dirs,
-                       seq(from = first_subject_pergroup[ii], 
-                        to = first_subject_pergroup[ii] + (n_pergroup - 1), 
-                        by = 1))
-  }
-  
-  if (n_sessions == 1) {
-    for (ii in 1:length(sub_dirs)) {
-      dir.create(here::here(home_dir, study_name, 'data', 'rawdata', 
-                            paste('sub-', sub_dirs[ii], sep = '')))
-    }
-  } else if (n_sessions > 1) {
-    for (ii in 1:length(sub_dirs)) {
-      dir.create(here::here(home_dir, study_name, 'data', 'rawdata', 
-                            paste('sub-', sub_dirs[ii], sep = '')))
-      for (jj in 1:n_sessions) {
-       dir.create(here::here(home_dir, study_name, 'data', 'rawdata', 
-                             paste('sub-', sub_dirs[ii], sep = ''),
-                             paste('ses-', session_labels[jj], sep = ''))) 
-      }
-    }
-  }
-  
   ###### create project subdirectory - doc
   dir.create(here::here(home_dir, study_name, 'doc'))
   if (verbose == TRUE) {
@@ -83,17 +50,10 @@ setup_project <- function(study_name, home_dir,
     message(paste('created src subdirectory: ', study_name, '/src', ' ...', sep = ''))
   }
   
-  ###### build experimentR-specific json file
+  ###### build studymanageR-specific json file
   project_info <- list(
     study_name = study_name,
-    home_dir = here::here(home_dir, study_name), 
-    n_sessions = n_sessions,
-    session_labels = session_labels,
-    n_groups = n_groups,
-    group_labels = group_labels,
-    n_pergroup = n_pergroup,
-    n_total = n_pergroup*n_groups,
-    participants = paste('sub-', sub_dirs, sep = '')
+    root_dir = here::here(home_dir, study_name)
   )
   project_info <- rjson::toJSON(project_info)
   write(project_info, here::here(home_dir, study_name, paste(study_name, '_info.json', sep = '')))
